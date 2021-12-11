55533a4ca5e7082f5f5e32e126e2ebc78e28da0d
MBanis <miles@russello.net>
2021-12-11 12:55:35 -0500

    small change to corr plot

--- Preprocessing/correlation.py
+++ Preprocessing/correlation.py
@@ -4,8 +4,6 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('../data/data.csv')
 
-print(data.loc[0])
-
 data = data.iloc[:, 1:]
 data = data.drop('danceability', 1)
 
@@ -13,8 +11,6 @@ print(data['energy'].value_counts())
 
 corr = data.corr()
 
-print(corr)
-
-sns.set(rc = {'figure.figsize':(15,15)})
-sns.heatmap(corr, annot=True)
+sns.set(rc={'figure.figsize': (15, 15)})
+sns.clustermap(corr, annot=True, cmap="vlag")
 plt.show()
